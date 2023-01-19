0fa83e8c71f47b2c464f5a0c44d2b546201e98c8
Kim YoungHyun <ikimyh22@naver.com>
2023-01-19 19:05:32 +0900

    컨트롤러 추가입니다.

--- yougo/src/main/java/com/test/yougo/YougoApplication.java
+++ yougo/src/main/java/com/test/yougo/YougoApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YougoApplication {
 
+	@Controller
 	public static void main(String[] args) {
 		SpringApplication.run(YougoApplication.class, args);
+		
+		
 	}
 
 }
