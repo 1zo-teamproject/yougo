4a7773ffa89554a9c3cb8e693348ae965763135c
Kim YoungHyun <ikimyh22@naver.com>
2023-01-19 16:52:11 +0900

    1조 팀프로젝트 yougo.gg

---
+++ yougo/src/main/java/com/test/yougo/YougoApplication.java
@@ -0,0 +1,13 @@
+package com.test.yougo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YougoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YougoApplication.class, args);
+	}
+
+}
---
+++ yougo/src/test/java/com/test/yougo/YougoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.yougo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YougoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
