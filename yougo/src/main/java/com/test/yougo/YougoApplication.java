79cdab8a6c5255a6d0dda3da55409ad16afcf7d6
Ahn <Ahn@DESKTOP-JBGS5MJ>
2023-01-20 14:35:40 +0900

    컨트롤러 삭제

--- yougo/src/main/java/com/test/yougo/YougoApplication.java
+++ yougo/src/main/java/com/test/yougo/YougoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YougoApplication {
 
-	@Controller
 	public static void main(String[] args) {
 		SpringApplication.run(YougoApplication.class, args);
 		
